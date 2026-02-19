3aef081f186afc7c21c19a5ed6fa4c3ddc8c65d7
Herum shaikh <erum20203@gmail.com>
2026-02-19 23:36:34 +0530

    completed numbers task

---
+++ numbers.py
@@ -0,0 +1,26 @@
+
+result = format(145, 'o')
+
+print("Octal representation of 145 is:", result)
+
+radius = 84
+pi = 3.14
+
+area = pi * radius * radius
+
+print("Area of pond:", area)
+
+
+water_per_square_meter = 1.4
+
+total_water = area * water_per_square_meter
+
+
+print("Total water in pond (liters):", int(total_water))
+
+
+distance = 490   
+time = 7 * 60    
+speed = distance / time
+
+print("Speed in m/s:", int(speed))
