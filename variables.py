224d79fcbc1cd61cecb47db3886410d1b768fbe5
Herum shaikh <erum20203@gmail.com>
2026-02-19 23:20:07 +0530

    completed variables task

---
+++ variables.py
@@ -0,0 +1,8 @@
+pi=22/7
+print("value of pi:",pi)
+print("value of pi:",type(pi))
+principal =1000
+rate = 5
+time = 6
+simple_interest =(principal*rate*time)/100
+print("simple intrest is :",simple_interest)
